a768cebe56b233ab05eec37951b398e14bc6b510
ADDA Admin <admin@omnes.cl>
2026-02-28 19:04:14 -0300

    Configure Firebase for initial release

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,14 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace with your actual Firebase config
+// Firebase configuration
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA1g6UDmrcWXGqkKMdmONvjWPhp3-85JHY",
+  authDomain: "imperia-42291.firebaseapp.com",
+  projectId: "imperia-42291",
+  storageBucket: "imperia-42291.firebasestorage.app",
+  messagingSenderId: "757447533395",
+  appId: "1:757447533395:web:ff0a13aa7111d0321237a6",
+  measurementId: "G-M62MJZTT4L"
 };
 
 const app = initializeApp(firebaseConfig);
